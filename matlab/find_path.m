05fead0d042ba5ee5866d9fc7bcdd50773d6d86d
idawatibustan <idawati.bustan@gmail.com>
2018-04-10 21:08:01 +0800

    Update find path with dynamic obstacle reading

--- matlab/find_path.m
+++ matlab/find_path.m
@@ -4,79 +4,102 @@ close all
 
 %%
 %----------------------------------------------------------------
-%Paramter
+%Drone params
 %----------------------------------------------------------------
-x_obstacle_1 = 1;
-y_obstacle_1 = 1;
+%maximum velocities
+v_max_front = 3.1 ;
+v_max_up = 2.2 ;
+v_min_up = -0.55 ;
 
-x_obstacle_2 = -1;
-y_obstacle_2 = 1;
+%maximum accelerations
+acc_max_front = 2.8 ;
+acc_max_up =  2 ;
+acc_min_up = -0.5 ;
 
-x_obstacle_3 = 0.5;
-y_obstacle_3 = -0.5;
+% maximum jerks
+jerk_max_front = 7.1 ;
+jerk_max_up = 5.0 ;
+jerk_min_up = -5.0 ;
 
+%%
+%----------------------------------------------------------------
+%Map params
+%----------------------------------------------------------------
 width = 6;
 height = 6;
 resolution = 0.01;
 
-nodes = 500;
-ConnectionDistance = 0.8;
+% prm param
+nodes = 1500;
+ConnectionDistance = 1;
 
-r_constraint_circle = 0.4;
+trans = [3 3];
+start = [-1.5 1.5] + trans;
+goal_1 = [2 0] + trans;
+goal_2 = [-1.5 -1.5] + trans;
+
+r_constraint_circle = 0.3;
 x_constraint_circle = 3;
 y_constraint_circle = 4.5;
 
-start = [1.5 1.5];
-goal_1 = [3 5];
-goal_2 = [4.5 1.5];
+%%
+%----------------------------------------------------------------
+%Read obstacle data
+%----------------------------------------------------------------
+obs_fileID = fopen('obs','r');
+obs_format = '%f %f';
+obs_size = [2 inf];
+obs = fscanf(obs_fileID, obs_format, obs_size);
+num_obs = length(obs);
+for obs_i = 1:length(obs)
+    % populate obstacle
+    obs(:, obs_i) = obs(:, obs_i) + 3;
+    % disp(obs(:, obs_i));
+end
 
 %%
 %----------------------------------------------------------------
 %Map Creation
 %----------------------------------------------------------------
-
 map = robotics.BinaryOccupancyGrid(width,height,1/resolution)
 
-x_obstacle_1 = x_obstacle_1 + 3;
-y_obstacle_1 = y_obstacle_1 + 3;
+% populate obstacle
+for obs_i = 1:length(obs)
+    setOccupancy(map, obs(:, obs_i)', 1);
+end
+inflate(map,.6);
 
-x_obstacle_2 = x_obstacle_2 + 3;
-y_obstacle_2 = y_obstacle_2 + 3;
-
-x_obstacle_3 = x_obstacle_3 + 3;
-y_obstacle_3 = y_obstacle_3 + 3;
-
-setOccupancy(map,[x_obstacle_1 y_obstacle_1], 1);
-setOccupancy(map,[x_obstacle_2 y_obstacle_2], 1);
-setOccupancy(map,[x_obstacle_3 y_obstacle_3], 1);
-inflate(map,.6)
-
+% constraint_circle
 x = (0:resolution/1.1:width);
 y = (0:resolution/1.1:height);
 
-for i = 1:(1.1*width/resolution)
-	for j = 1:(1.1*height/resolution)
-		if sqrt((x(i)- x_constraint_circle)^2+(y(j)-y_constraint_circle-resolution/2)^2) < r_constraint_circle ...
-			&& y(j) > y_constraint_circle+0.5*r_constraint_circle
-			setOccupancy(map, [x(i) y(j)], 1)
-		end
-	end
-end
+ for i = 1:(1.1*width/resolution)
+     for j = 1:(1.1*height/resolution)
+        if sqrt((x(i)- x_constraint_circle)^2+(y(j)-y_constraint_circle-resolution/2)^2) < r_constraint_circle ...
+        && y(j) > y_constraint_circle+0.5*r_constraint_circle
+            setOccupancy(map, [y(j) x(i)], 1)
+         end
+     end
+ end
+
+figure
+show(map)
 
 %%
 %------------------------------------------------------------------
 %PRM
 %------------------------------------------------------------------
-planner = robotics.PRM(map,nodes)
+planner = robotics.PRM(map,nodes);
 planner.ConnectionDistance = ConnectionDistance;
+planner
 
 path1 = findpath(planner,start,goal_1);
-figure()
-show(planner)
+ figure()
+ show(planner)
 path2 = findpath(planner,goal_1,goal_2);
-figure()
-show(planner)
-grid on
+ figure()
+ show(planner)
+ grid on
 
 %%
 %------------------------------------------------------------------
@@ -84,22 +107,13 @@ grid on
 %------------------------------------------------------------------
 
 path = vertcat(start,path1(4:end-2,1:end),goal_1,path2(3:end-2,1:end),goal_2);
-
 path_x = path(1:end,1);
 path_y = path(1:end,2);
 
-
-delta = 1:.1:size(path_x);
-xq = spline(1:size(path_x),path_x,delta) ; % - 3.*ones(size(delta));
-yq = spline(1:size(path_x),path_y,delta) ; %- 3.*ones(size(delta));
-figure
+figure('Name', 'Path Generated');
 hold on
-plot(xq,yq);
 scatter(path_x',path_y');
 hold off
+path = path - 3.*ones(size(path)) ;
 
-shape = size(xq) ;
-
-smooth_path = horzcat(xq', yq') - 3.*ones(shape(2),2) ;
-
-clearvars -except smooth_path
+clearvars -except path
